491f3f17b5a7421d0f3fbf31f7c8cfcfc1cfd297
hzhu <hzhu@hzhu.(none)>
2014-03-08 15:58:18 -0500

    Added quit and also some important classes.

--- game.py
+++ game.py
@@ -1,16 +1,43 @@
 import sys, pygame, pygame.mixer
 from pygame.locals import *
 
-from pygame.init()
+pygame.init()
 
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
 size = width, height = 600, 400
+screen = pygame.display.set_mode(size)
 black = 0,0,0
+
+class PathModel:
+    pass
+
+class Bob:
+    pass
+
+class Blocks:
+    pass
+
+class PathBuildView:
+    pass
+
+class PathPlayView:
+    pass
+
+class PathKeyboardController:
+    pass
+
+class PathMouseController:
+    pass
+
 
 while True:
     # Checks for any type of user input.
-    for event in pygame.events.get():
-        pass
+    for event in pygame.event.get():
+        # Exits script when exit button is pressed.
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            sys.exit()
 
     # Fill background with black to wipe any pre-existing states.
     screen.fill(black)
